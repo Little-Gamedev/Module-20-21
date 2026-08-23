b83f6a8ad5c013d192e8fa6314f880bc7f4592cb
Максим <telefonflymax@gmail.com>
2026-08-23 19:07:30 +0300

    Initial commit

---
+++ Assets/_Project/Scripts/Drag/Dragging.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class Dragging : MonoBehaviour
+{
+    private Transform _draggedObject;
+    private Vector3 _inititalPosition;
+    //public void StartDragging()
+
+}
---
+++ Assets/_Project/Scripts/Items/Item.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Item : MonoBehaviour, IDraggable
+{
+    [SerializeField] private Dragging _dragging;
+
+    private void Awake()
+    {
+
+    }
+
+    public void Drag(Dragging dragging)
+    {
+
+    }
+}
